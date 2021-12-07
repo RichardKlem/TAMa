5b1d7880815dcc8ba49de8636acd577568ecb1f2
Richard Klem <48335467+RichardKlem@users.noreply.github.com>
2021-12-08 00:59:17 +0100

    Main Activity work in progress.

--- app/src/main/java/com/example/tama/MainActivity.kt
+++ app/src/main/java/com/example/tama/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.tama
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
@@ -18,8 +18,8 @@ import com.example.tama.ui.events.EventsFragment
 import com.example.tama.ui.locations.LocationFragment
 import com.example.tama.ui.settings.SettingsFragment
 import com.example.tama.worker.NotificationWorker
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_location.*
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
@@ -27,10 +27,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private val fragmentManager = supportFragmentManager
-    private val eventFragment = EventsFragment()
-    private val locationFragment = LocationFragment()
+    private lateinit var eventFragment: EventsFragment
+    private lateinit var locationFragment: LocationFragment
+
+    //    private lateinit var settingsFragment: SettingsFragment
     private val settingsFragment = SettingsFragment()
-    private var activeFragment: Fragment = locationFragment
+    private lateinit var activeFragment: Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,22 +49,26 @@ class MainActivity : AppCompatActivity() {
             )
         )
 
-        val navHostFragment: NavHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navHostFragment: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
         val navController: NavController = navHostFragment.navController
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        fragmentManager.beginTransaction().apply {
-            add(R.id.container, locationFragment, getString(R.string.title_events))
-            add(R.id.container, eventFragment, getString(R.string.title_locations)).hide(eventFragment)
-            add(R.id.container, settingsFragment, getString(R.string.settings)).hide(settingsFragment)
-        }.commit()
-        initListeners()
+
+//        fragmentManager.beginTransaction().apply {
+//            add(R.id.container, locationFragment, getString(R.string.title_events))
+//            add(R.id.container, eventFragment, getString(R.string.title_locations)).hide(eventFragment)
+//            add(R.id.container, settingsFragment, getString(R.string.settings)).hide(settingsFragment)
+//        }.commit()
+        println("MAIN BEFORE")
+//        initListeners()
         nav_view.itemIconTintList = null
 
         // set notification worker
-        val workerRequest = PeriodicWorkRequestBuilder<NotificationWorker>(2, TimeUnit.HOURS).build()
+        val workerRequest =
+            PeriodicWorkRequestBuilder<NotificationWorker>(2, TimeUnit.HOURS).build()
         WorkManager.getInstance(applicationContext).enqueue(workerRequest)
     }
 
@@ -74,42 +80,45 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
         if (id == R.id.goto_settings) {
-//            fragmentManager.beginTransaction().replace(activeFragment.id, settingsFragment)
-//            fragmentManager.beginTransaction().addToBackStack(null)
-//            fragmentManager.beginTransaction().commit()
-            fragmentManager.beginTransaction().hide(activeFragment).show(settingsFragment).commit()
-            activeFragment = settingsFragment
-            btnAddLocation.hide()
-            return true
+////            fragmentManager.beginTransaction().replace(activeFragment.id, settingsFragment)
+////            fragmentManager.beginTransaction().addToBackStack(null)
+////            fragmentManager.beginTransaction().commit()
+//            fragmentManager.beginTransaction().hide(activeFragment).show(settingsFragment).commit()
+//            activeFragment = settingsFragment
+//            btnAddLocation.hide()
+//            return true
+            val switchActivityIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(switchActivityIntent)
         }
+
         return super.onOptionsItemSelected(item)
     }
 
-    private fun initListeners() {
-        nav_view.setOnItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.navigation_events -> {
-//                    fragmentManager.beginTransaction().replace(activeFragment.id, eventFragment)
-//                    fragmentManager.beginTransaction().addToBackStack(null)
-//                    fragmentManager.beginTransaction().commit()
-                    fragmentManager.beginTransaction().hide(activeFragment).show(eventFragment).commit()
-                    activeFragment = eventFragment
-                    btnAddLocation.hide()
-                    true
-                }
-
-                R.id.navigation_dashboard -> {
-//                    fragmentManager.beginTransaction().replace(activeFragment.id, locationFragment)
-//                    fragmentManager.beginTransaction().addToBackStack(null)
-//                    fragmentManager.beginTransaction().commit()
-                    fragmentManager.beginTransaction().hide(activeFragment).show(locationFragment).commit()
-                    activeFragment = locationFragment
-                    btnAddLocation.show()
-                    true
-                }
-
-                else -> false
-            }
-        }
-    }
+//    private fun initListeners() {
+//        nav_view.setOnItemSelectedListener { menuItem ->
+//            when (menuItem.itemId) {
+//                R.id.navigation_events -> {
+////                    fragmentManager.beginTransaction().replace(activeFragment.id, eventFragment)
+////                    fragmentManager.beginTransaction().addToBackStack(null)
+////                    fragmentManager.beginTransaction().commit()
+//                    fragmentManager.beginTransaction().hide(activeFragment).show(eventFragment).commit()
+//                    activeFragment = eventFragment
+//                    btnAddLocation.hide()
+//                    true
+//                }
+//
+//                R.id.navigation_dashboard -> {
+////                    fragmentManager.beginTransaction().replace(activeFragment.id, locationFragment)
+////                    fragmentManager.beginTransaction().addToBackStack(null)
+////                    fragmentManager.beginTransaction().commit()
+//                    fragmentManager.beginTransaction().hide(activeFragment).show(locationFragment).commit()
+//                    activeFragment = locationFragment
+//                    btnAddLocation.show()
+//                    true
+//                }
+//
+//                else -> false
+//            }
+//        }
+//    }
 }
