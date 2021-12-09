b6fddf388fc638ee1047ddd0028498bfbd3d8548
cattylex <xpesko00@vutbr.cz>
2021-12-09 20:42:10 +0100

    light refactor

--- app/src/main/java/com/example/tama/ui/events/EventsFragment.kt
+++ app/src/main/java/com/example/tama/ui/events/EventsFragment.kt
@@ -1,9 +1,7 @@
 package com.example.tama.ui.events
 
-import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,18 +9,12 @@ import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.tama.R
-import com.example.tama.data.entity.Cleaning
 import com.example.tama.databinding.FragmentEventsBinding
 import com.example.tama.helpers.getEvents
-import kotlinx.android.synthetic.main.events_on_main.view.*
 import kotlinx.android.synthetic.main.fragment_events.*
 import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
-import java.time.ZoneId
-import kotlin.math.log
 
 
 class EventsFragment : Fragment() {
@@ -55,23 +47,6 @@ class EventsFragment : Fragment() {
 
         rvEventItems.adapter = eventsAdapter
         rvEventItems.layoutManager = LinearLayoutManager(this.context)
-//
-//        val event = Event("Bozetechova", getString(R.string.date), getString(R.string.startTime), getString(R.string.endTime))
-//        eventsAdapter.addEvent(event)
-//        val event2 = Event("Ceska", getString(R.string.date_early), getString(R.string.startTime), getString(R.string.endTime))
-//        eventsAdapter.addEvent(event2)
-//        val event3 = Event("Grohova", getString(R.string.date), getString(R.string.startTime), getString(R.string.endTime))
-//        eventsAdapter.addEvent(event3)
-//        val event4 = Event("Konecneho namesti", getString(R.string.date_early), getString(R.string.startTime), getString(R.string.endTime))
-//        eventsAdapter.addEvent(event4)
-//        val event5 = Event("Hlavni nadrazi", getString(R.string.date), getString(R.string.startTime), getString(R.string.endTime))
-//        eventsAdapter.addEvent(event5)
-//        val event6 = Event("Antoninska", getString(R.string.date_early), getString(R.string.startTime), getString(R.string.endTime))
-//        eventsAdapter.addEvent(event6)
-//        val event7 = Event("Uvoz", getString(R.string.date), getString(R.string.startTime), getString(R.string.endTime))
-//        eventsAdapter.addEvent(event7)
-//        val event8 = Event("Masarykova", getString(R.string.date_early), getString(R.string.startTime), getString(R.string.endTime))
-//        eventsAdapter.addEvent(event8)
 
         val formatter = DateTimeFormatter.ofPattern("d. M. yyyy, HH.mm")
 
