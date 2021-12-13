69adff74a5cc45e0410833de71ea84787b0b5524
cattylex <xpesko00@vutbr.cz>
2021-12-13 14:45:13 +0100

    back from settings

--- app/src/main/java/com/example/tama/ui/events/EventsCardAdapter.kt
+++ app/src/main/java/com/example/tama/ui/events/EventsCardAdapter.kt
@@ -1,7 +1,7 @@
 package com.example.tama.ui.events
 
 import android.os.Build
-//import android.util.Log
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
